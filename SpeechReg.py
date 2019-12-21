e102cf32b6324f03d92ea41b05b175498f3dd2e7
Navneet Singh Negi <33743031+navneetsn18@users.noreply.github.com>
2019-12-22 03:07:09 +0530

    Update SpeechReg.py

--- SpeechReg.py
+++ SpeechReg.py
@@ -1,12 +1,13 @@
-import speech_recognition as speech
+import speech_recognition as speech 
 
 rec=speech.Recognizer()
 
 with speech.Microphone() as source:
-    print("Please speak something: ")
+    print("Please say something: ")
     audio=rec.listen(source)
     try:
-        result=rec.recorgnize_google(audio)
+        result=rec.recognize_google(audio)
         print("You just said: {}".format(result))
     except:
-        print("Sorry! Unable to understand what you just said!")
+        print("Sorry! Unable to understand what you just said.")
+    
